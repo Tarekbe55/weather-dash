f322e191a8c64d4109e3f8c979299277faf60863
Tarek Behairy <tbtarek@gmail.com>
2021-01-17 15:44:18 -0500

    variable assignment in javascript file

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+//variable to store the searched city
+var city = "";
+// variables assignment
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty = $("#humidity");
+var currentWSpeed = $("#wind-speed");
+var currentUvindex = $("#uv-index");
