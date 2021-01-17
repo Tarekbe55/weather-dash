a4a327379999ba2309bec370cf91d83e02bb7307
Tarek Behairy <tbtarek@gmail.com>
2021-01-17 16:18:10 -0500

    set up structure for 5 day forecast

--- assets/js/index.js
+++ assets/js/index.js
@@ -95,4 +95,21 @@ function forecast(cityid) {
   $.ajax({
     url: forcastURL,
     method: "GET"
-  })
+  }).then(function (response) {
+
+    for (i = 0; i < 5; i++) {
+      var date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
+      var iconcode = response.list[((i + 1) * 8) - 1].weather[0].icon;
+      var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+      var tempK = response.list[((i + 1) * 8) - 1].main.temp;
+      var tempF = (((tempK - 273.5) * 1.80) + 32).toFixed(2);
+      var humidity = response.list[((i + 1) * 8) - 1].main.humidity;
+
+      $("#Date" + i).html(date);
+      $("#Img" + i).html("<img src=" + iconurl + ">");
+      $("#Temp" + i).html(tempF + "&#8457");
+      $("#Humidity" + i).html(humidity + "%");
+    }
+
+  });
+}
